9b853cc8e0d3827856ab9dc81c166575dede8a9c
Het Kothari <projectecom0@gmail.com>
2025-02-03 18:21:01 +0530

    removed errors from app.py code for deployment

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, jsonify, send_from_directory
+from flask import Flask, render_template, request, flash, jsonify, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import os
 from dotenv import load_dotenv
 from datetime import datetime
 import json
-import psycopg2
 from openai import OpenAI
 
-
+# Load environment variables
 load_dotenv()
 model = os.getenv("MODEL")
 api_key = os.getenv("API_KEY")
 client = OpenAI(api_key=api_key)
-google_client_id = os.getenv("GOOGLE_CLIENT_ID")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
@@ -21,19 +19,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-SECRET_KEY = 'many random bytes'
-app.secret_key = 'many random bytes'
+app.secret_key = os.urandom(24)
 
-
-conn = psycopg2.connect(
-    database="gpt_prompt-responses",
-    user="postgres",
-    password="root",
-    host="localhost"
-)
-cursor = conn.cursor()
-
-
+# User credentials model
 class UserCreds(db.Model):
     __tablename__ = 'new_user_creds'
     sNo = db.Column(db.Integer, primary_key=True)
@@ -48,8 +36,7 @@ class UserCreds(db.Model):
         self.password = password
         self.google_id = google_id
 
-
-# creates db table based on the email
+# Create user-specific table
 def create_db_table(email):
     table_name = f"{email.replace('@', '_').replace('.', '_')}_data"
 
@@ -71,7 +58,7 @@ def create_db_table(email):
 
     return Table
 
-
+# Routes
 @app.route("/", methods=['POST', 'GET'])
 def signup_page():
     if request.method == 'POST':
@@ -81,21 +68,17 @@ def signup_page():
             name = data.get("given_name")
             google_id = data.get("sub")
 
-            # Check for existing email
             existing_email = UserCreds.query.filter_by(email=email).first()
 
-
-            if existing_email is not None and email == existing_email[0]:
+            if existing_email:
                 flash('USER ALREADY EXISTS!', 'error')
                 return jsonify({"success": True, "redirect_url": url_for('login_page', username=name)})
             else:
-                cred_table = UserCreds(name=name, email=email, google_id=google_id)
-                db.session.add(cred_table)
+                new_user = UserCreds(name=name, email=email, google_id=google_id)
+                db.session.add(new_user)
                 db.session.commit()
 
-                # Create user-specific table
                 create_user_table(email)
-
                 flash('Login Successful!', 'success')
                 return jsonify({"success": True, "redirect_url": url_for('user_endpoint', username=name)})
         else:
@@ -104,25 +87,20 @@ def signup_page():
             unhashed_password = request.form.get("password")
             password = bcrypt.generate_password_hash(unhashed_password).decode('utf-8')
 
-            # Check for existing email
-            cursor.execute("SELECT email FROM public.new_user_creds WHERE email = %s", (email,))
-            existing_email = cursor.fetchone()
-            # print(existing_email is not None, existing_email[0], email)
+            existing_email = UserCreds.query.filter_by(email=email).first()
 
-            if existing_email is not None and email == existing_email[0]:
+            if existing_email:
                 flash("USER ALREADY EXISTS!", "error")
                 return redirect(url_for('signup_page'))
             else:
-                cred_table = UserCreds(name=name, email=email, password=password)
-                db.session.add(cred_table)
+                new_user = UserCreds(name=name, email=email, password=password)
+                db.session.add(new_user)
                 db.session.commit()
 
-                # Create user-specific table
                 create_user_table(email)
                 flash('Registered Successfully!', 'success')
                 return redirect(url_for('login_page'))
     return render_template('signup.html')
-
 
 @app.route("/login", methods=["POST", "GET"])
 def login_page():
@@ -133,14 +111,14 @@ def login_page():
             password = data.get("ud")
 
             user = UserCreds.query.filter_by(email=email).first()
-            # print(email, user.email)
-            if user is not None and email == user.email:
+
+            if user and bcrypt.check_password_hash(user.password, password):
                 username = user.name
-                flash('Login Successful!', 'error')
+                flash('Login Successful!', 'success')
                 return jsonify({"success": True, "redirect_url": url_for('user_endpoint', username=username)})
-            elif user is None:
-                flash('USER NOT FOUND!', 'error')
-                return jsonify({"success": True, "redirect_url": url_for('signup_page')})
+            else:
+                flash('Invalid Credentials, Please try again!', 'error')
+                return jsonify({"success": False, "redirect_url": url_for('login_page')})
         else:
             email = request.form.get("email")
             password = request.form.get("password")
@@ -155,7 +133,6 @@ def login_page():
                 flash('Invalid Credentials, Please try again!', 'error')
 
     return render_template('login.html')
-
 
 @app.route("/<username>", methods=['POST', 'GET'])
 def user_endpoint(username):
@@ -187,8 +164,8 @@ def user_endpoint(username):
 
         timestamp = datetime.now()
 
-        create_table = table_model(prompt=prompt_data, responses=result, history=history, timestamp=timestamp)
-        db.session.add(create_table)
+        create_entry = table_model(prompt=prompt_data, responses=result, history=history, timestamp=timestamp)
+        db.session.add(create_entry)
         db.session.commit()
 
         return redirect(url_for('user_endpoint', username=username, result=result, history=json.dumps(history)))
@@ -199,44 +176,21 @@ def user_endpoint(username):
     return render_template('interfaceTesting.html', result=result, username=username, history=history,
                            chat_history=chat_history, timestamp=(datetime.now()).strftime('%d-%m-%Y %H:%M:%S'))
 
-
-@app.route('/dbshow/<username>', methods=["POST", "GET"])
-def show_database(username):
-    user = UserCreds.query.filter_by(name=username).first()
-    if not user:
-        return "User not found", 404
-
-    email = user.email
-    data_name = f"{email.replace('@', '_').replace('.', '_')}_data"
-    query = f"SELECT * FROM public.{data_name}"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    timestamp = datetime.now()
-
-    output = []
-    for row in result:
-        output.append({'prompt': row[1], 'responses': row[2], 'timestamp': row[4]})
-
-    return render_template('db.html', output=output, username=username, timestamp=timestamp)
-
-
 @app.route("/navigate_pages", methods=['POST'])
 def navigate_pages():
     selected_users = request.form.get("users")
     return redirect(url_for('user_endpoint', username=selected_users))
-
-
-def create_user_table(email):
-    table_model = create_db_table(email)
-    with app.app_context():
-        db.create_all()
-
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+# Create user table function
+def create_user_table(email):
+    table_model = create_db_table(email)
+    with app.app_context():
+        db.create_all()
 
 if __name__ == "__main__":
     with app.app_context():
@@ -246,4 +200,4 @@ if __name__ == "__main__":
             create_user_table(user.email)
     app.config['ENV'] = 'production'
     app.config['DEBUG'] = False
-
+    app.run()
